efd1ebee19ba529258efde716b79fa396730fc98
tkop <2306758816@qq.com>
2024-03-26 20:38:22 +0800

    Update customizex86.sh

--- customizex86.sh
+++ customizex86.sh
@@ -1,1 +1,23 @@
-customize.sh
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.5.2/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. Replace with JerryKuKu’s Argon
+#rm openwrt/package/lean/luci-theme-argon -rf
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/Xiaomi/g' openwrt/package/base-files/files/bin/config_generate
+
+sed -i 's/\/bin\/ash/\/bin\/bash/' openwrt/package/base-files/files/etc/passwd
